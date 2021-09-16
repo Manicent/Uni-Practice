195e5d228a6c914887ca9017b6b6e90855172e79
Mani <46489166+Manicent@users.noreply.github.com>
2021-09-16 16:12:00 +0530

    Create EXP 2 (Read and Print Student Info).cpp

---
+++ SEM III/OODP/EXP 2 (Read and Print Student Info).cpp
@@ -0,0 +1,58 @@
+/*
+Q:  Read and Print Student Information Class Example Program in C++
+*/
+
+#include<iostream>
+#include<string>
+#define STUDENT_NAME_LIMIT 30
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class StudentInfo
+{
+    private:
+    char name[STUDENT_NAME_LIMIT], section;
+    int grade, rollno;
+
+    public:
+    void ReadInfo();
+    void PrintInfo();
+};
+
+void StudentInfo::ReadInfo()
+{
+    cout<<"Enter the student info below!"<<endl;
+
+    cout<<"Enter the name: ";
+    cin.getline(name, STUDENT_NAME_LIMIT);
+    cout<<"Enter the class: ";
+    cin>>grade;
+    cout<<"Enter the section: ";
+    cin>>section;
+    cout<<"Enter the roll number: ";
+    cin>>rollno;
+}
+
+void StudentInfo::PrintInfo()
+{
+    cout<<"Student Info for "<<name<<"!"<<endl;
+
+    cout<<"Class:\t\t"<<grade<<endl;
+    cout<<"Section:\t"<<section<<endl;
+    cout<<"Roll Number:\t"<<rollno<<endl;
+}
+
+int main()
+{
+    StudentInfo stuobj;
+
+    stuobj.ReadInfo();
+
+    cout<<endl;
+
+    stuobj.PrintInfo();
+
+    return 0;
+}
